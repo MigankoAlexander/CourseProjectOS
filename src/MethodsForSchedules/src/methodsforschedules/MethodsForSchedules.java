40deb6f11cfeb730b0af154c4e3fedc180d72889
VitalyIvanov <vitalyivanov88@gmail.com>
2012-11-29 22:47:39 +0400

    math methods signature added

--- src/MethodsForSchedules/src/methodsforschedules/MethodsForSchedules.java
+++ src/MethodsForSchedules/src/methodsforschedules/MethodsForSchedules.java
@@ -4,17 +4,27 @@
  */
 package methodsforschedules;
 
+import java.awt.geom.Point2D;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Miganko
  */
 public class MethodsForSchedules {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-     
-        
+    public static List<Point2D.Double> interpolate(List<Point2D.Double> points, int size) {
+        List<Point2D.Double> p = new ArrayList<>();
+        p.add(new Point2D.Double(1.0, 2.0));
+        p.add(new Point2D.Double(2.0, 4.0));
+        return p;
+    }
+
+    public static List<Point2D.Double> approximate(List<Point2D.Double> points, int size) {
+        List<Point2D.Double> p = new ArrayList<>();
+        p.add(new Point2D.Double(1.0, 2.0));
+        p.add(new Point2D.Double(2.0, 4.0));
+        return p;
     }
 }
