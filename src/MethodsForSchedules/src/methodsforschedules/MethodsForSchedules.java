679192eb86a0d49ea6ed47543573fab4b7299fde
Miganko <Miganko@Miganko-PC.Dlink>
2012-12-05 00:25:08 +0400

    interpolate method added

--- src/MethodsForSchedules/src/methodsforschedules/MethodsForSchedules.java
+++ src/MethodsForSchedules/src/methodsforschedules/MethodsForSchedules.java
@@ -16,15 +16,32 @@ public class MethodsForSchedules {
 
     public static List<Point2D.Double> interpolate(List<Point2D.Double> points, int size) {
         List<Point2D.Double> p = new ArrayList<>();
-        p.add(new Point2D.Double(1.0, 2.0));
-        p.add(new Point2D.Double(2.0, 4.0));
+        double y = 0.0;
+        double x = points.get(0).x;
+        double drob = 1.0;
+        double step = (points.get(points.size() - 1).x - points.get(0).x) / size;
+        for (int index = 0; index <= size; index++) {
+            for (int i = 0; i < points.size(); i++) {
+                for (int j = 0; j < points.size(); j++) {
+                    if (j != i) {
+                        drob *= (x - points.get(j).x) / (points.get(i).x - points.get(j).x);
+                    }
+                }
+                y += points.get(i).y * drob;
+                drob = 1.0;
+            }
+            p.add(new Point2D.Double(x, y));
+            x += step;
+            y = 0.0;
+            
+        }
         return p;
     }
 
     public static List<Point2D.Double> approximate(List<Point2D.Double> points, int size) {
         List<Point2D.Double> p = new ArrayList<>();
         p.add(new Point2D.Double(1.0, 2.0));
-        p.add(new Point2D.Double(2.0, 4.0));
+        p.add(new Point2D.Double(2.0, 5.0));
         return p;
     }
 }
