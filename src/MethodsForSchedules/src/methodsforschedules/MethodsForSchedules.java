ba3ea0f29390075ba50229fad3dbce75dfb9dac4
Miganko <Miganko@Miganko-PC.Dlink>
2012-12-16 23:30:36 +0400

    add ordinary least squares method

--- src/MethodsForSchedules/src/methodsforschedules/MethodsForSchedules.java
+++ src/MethodsForSchedules/src/methodsforschedules/MethodsForSchedules.java
@@ -20,7 +20,7 @@ public class MethodsForSchedules {
         double x = points.get(0).x;
         double drob = 1.0;
         double step = (points.get(points.size() - 1).x - points.get(0).x) / size;
-        for (int index = 0; index <= size; index++) {
+        for (int index = 0; index < size; index++) {
             for (int i = 0; i < points.size(); i++) {
                 for (int j = 0; j < points.size(); j++) {
                     if (j != i) {
@@ -33,7 +33,7 @@ public class MethodsForSchedules {
             p.add(new Point2D.Double(x, y));
             x += step;
             y = 0.0;
-            
+
         }
         return p;
     }
@@ -42,6 +42,23 @@ public class MethodsForSchedules {
         List<Point2D.Double> p = new ArrayList<>();
         p.add(new Point2D.Double(1.0, 2.0));
         p.add(new Point2D.Double(2.0, 5.0));
+        return p;
+    }
+
+    public static List<Point2D.Double> ordinaryLeastSquares(List<Point2D.Double> points) {
+        List<Point2D.Double> p = new ArrayList<>();
+        double sum0 = 0;
+        double sum1 = 0;
+        for (int i = 0; i < points.size(); i++) {
+            sum0+=points.get(i).x*points.get(i).y;
+            sum1+=points.get(i).x*points.get(i).x;
+        }
+        double k = sum0/sum1;
+        double x = points.get(points.size()-1).x;
+        double y = x*k;
+        
+        p.add(new Point2D.Double(0.0, 0.0));
+        p.add(new Point2D.Double(x, y));
         return p;
     }
 }
